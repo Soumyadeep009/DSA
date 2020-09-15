055b661cbc8811b98c055e390006ef724c73c869
Soumyadeep Biswas <BiswasSoumyadeep@johndeere.com>
2020-09-15 13:24:47 +0530

    Logic about head and Tail described

--- src/com/soumyadeep/linkedList/CustomLinkedList.java
+++ src/com/soumyadeep/linkedList/CustomLinkedList.java
@@ -5,16 +5,33 @@ public class CustomLinkedList {
 	int value;
 	CustomLinkedList next;
 	int length;
-	
+
 	CustomLinkedList(int value) {
-		this.value=value;
-		this.next=null;
+		this.value = value;
+		this.next = null;
 		this.length++;
 	}
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		CustomLinkedList myLinkedList = new CustomLinkedList(10);
+
+		/** Head is the pointer to the first node of the List */
+		CustomLinkedList head = myLinkedList;
 		System.out.println(myLinkedList.value);
+
+		/**
+		 * Tail is the pointer to the end of the List. Since there is only one
+		 * node, so the first node is the last node.
+		 */
+		CustomLinkedList tail = head;
+
+		/**
+		 * Note if we change the value of head, the value of the first node
+		 * changes too.
+		 */
+		head.value = 20;//
+		System.out.println(myLinkedList.value);
+		System.out.println(head.value);
 		System.out.println(myLinkedList.length);
 	}
 
