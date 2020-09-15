25cc7b1181e3d895de97a04288176de99e3ba4eb
Soumyadeep Biswas <BiswasSoumyadeep@johndeere.com>
2020-09-15 14:04:44 +0530

    Add method implemented for Custom Linked List

--- src/com/soumyadeep/linkedList/CustomLinkedList.java
+++ src/com/soumyadeep/linkedList/CustomLinkedList.java
@@ -9,7 +9,24 @@ public class CustomLinkedList {
 	CustomLinkedList(int value) {
 		this.value = value;
 		this.next = null;
-		this.length++;
+	}
+	
+	public static CustomLinkedList add(CustomLinkedList list, int value){
+		
+		while(list.next!=null){
+			list=list.next;
+		}
+		CustomLinkedList newNode=new CustomLinkedList(value);
+		list.next=newNode;
+		return list;
+	}
+	
+	public static void printList(CustomLinkedList myList){
+		while(myList.next!=null){
+			System.out.print(myList.value+"->");
+			myList=myList.next;
+		}
+		System.out.println(myList.value);
 	}
 
 	public static void main(String args[]) {
@@ -17,7 +34,7 @@ public class CustomLinkedList {
 
 		/** Head is the pointer to the first node of the List */
 		CustomLinkedList head = myLinkedList;
-		System.out.println(myLinkedList.value);
+		//System.out.println(myLinkedList.value);
 
 		/**
 		 * Tail is the pointer to the end of the List. Since there is only one
@@ -29,10 +46,14 @@ public class CustomLinkedList {
 		 * Note if we change the value of head, the value of the first node
 		 * changes too.
 		 */
-		head.value = 20;//
+		/*head.value = 20;
 		System.out.println(myLinkedList.value);
 		System.out.println(head.value);
-		System.out.println(myLinkedList.length);
+		System.out.println(myLinkedList.length);*/
+		for(int i=1;i<=8;i++){
+			myLinkedList=CustomLinkedList.add(myLinkedList, i*2);
+		}
+		printList(head);
 	}
 
 }
